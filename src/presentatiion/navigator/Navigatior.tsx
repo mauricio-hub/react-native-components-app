29111d9f3f7387c001b73c3bffc7653fd110af76
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-07-17 07:54:52 -0500

    custom secction list

--- src/presentatiion/navigator/Navigatior.tsx
+++ src/presentatiion/navigator/Navigatior.tsx
@@ -6,6 +6,7 @@ import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
 import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
+import { CustomSecctionsListScreen } from '../screens/ui/CustomSecctionsListScreen';
 const Stack = createStackNavigator();
 export const Navigator = () =>{
   return (
@@ -21,6 +22,7 @@ export const Navigator = () =>{
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
       <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
+      <Stack.Screen name="CustomSectionListScreen" component={CustomSecctionsListScreen} />
 
     
     </Stack.Navigator>
